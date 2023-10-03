deb7849d7cb8a4b7cb1e2eabce28a43b513aa1eb
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-03 12:03:05 +0400

    feat: layout for mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -32,6 +34,14 @@ const Header = ({ children, className }: HeaderProps) => {
             className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
           >
             <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex-items-center hover:opacity-75 transition">
+            <HiHome size={20} className="text-black" />
+          </button>
+          <button className="rounded-full p-2 bg-white flex-items-center hover:opacity-75 transition">
+            <BiSearch size={20} className="text-black" />
           </button>
         </div>
       </div>
