2a0ccac67959e417d9642e5b80c72351f9a27297
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-03 10:45:03 +0400

    feat: basic routes data, mobile first layout prep

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,37 @@
+"use client";
+import { BiSearch } from "react-icons/bi";
+import Hihome from "react-icons/hi";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 interface SidebarProps {
   children: React.ReactNode;
 }
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
-  return <div>{children}</div>;
+  const pathname = usePathname();
+  const routes = useMemo(
+    () => [
+      {
+        icon: Hihome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+      },
+    ],
+    []
+  );
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black w-[300px] p-2 h-full ">
+        Sidebar Navigation
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
