6ea693e76665bf26ead4d88a23edb9f76e14a3bd
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-05 17:00:02 +0400

    feat: form state prep for upload modal, input init

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
+import { useState } from "react";
+import { Input } from "postcss";
 const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState();
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
       author: "",
@@ -19,7 +22,7 @@ const UploadModal = () => {
       uploadModal.onClose();
     }
   };
-
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {};
   return (
     <Modal
       title="Add song"
@@ -27,7 +30,14 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      Form
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+      </form>
     </Modal>
   );
 };
