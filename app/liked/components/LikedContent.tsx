5cc1cc914392d724b3043568960cb31d56185aec
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 01:40:15 +0400

    feat: liked button added for liked content

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
@@ -30,6 +31,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
     <div className="flex flex-col gap-y-2 w-full p-6 ">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <LikeButton songId={song.id} />
           <div className="flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
