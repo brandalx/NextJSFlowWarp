19cc82ac4888a38fcc322ea7df853939ce0bb3e8
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 01:59:06 +0400

    feat: useLoadSong hook

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import useGetSongById from "@/hooks/useGetSongById";
 import usePlayer from "@/hooks/usePlayer";
 
 const Player = () => {
   const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+
+  const songUrl = useLoadSongUrl;
 
   return <div>Player comp</div>;
 };
