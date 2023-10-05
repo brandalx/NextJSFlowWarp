9b2a9e3830cc1d6374a4ffadeced7a7d42182dbe
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-05 07:06:46 +0400

    feat: toast works

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ import useAuthModal from "@/hooks/useAuthModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
 import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -25,7 +26,9 @@ const Header = ({ children, className }: HeaderProps) => {
     // TODO: reset all songs
     router.refresh();
     if (error) {
-      console.log(error);
+      toast.error(error.message);
+    } else {
+      toast.success("Logged out");
     }
   };
   return (
