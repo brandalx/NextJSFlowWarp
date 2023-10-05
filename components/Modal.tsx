c5cfe3b04b65b9d8197a616719453f7c4358b56d
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-05 06:02:02 +0400

    feat: ModalProps

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,20 @@
-const Modal = () => {
+import * as Dialog from "@radix-ui/react-dialog";
+import { IoMdClose } from "react-icons/io";
+
+interface ModalProps {
+  isOpen: boolean;
+  onChange: (open: boolean) => void;
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onChange,
+  title,
+  description,
+  children,
+}) => {
   return <div>Modal component</div>;
 };
 
