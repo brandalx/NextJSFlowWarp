47c04442a37490c04e1ab805fe2c1216cab54e32
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 01:33:56 +0400

    feat:songs fetching, no songs handler

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,11 +1,30 @@
 "use client";
 
+import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 interface LikedContentProps {
   songs: Song[];
 }
 const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+
+  const { isLoading, user } = useUser();
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-3 w-full px-6 text-neutral-400 ">
+        No liked songs yet
+      </div>
+    );
+  }
   return <div>LikedContent</div>;
 };
 
