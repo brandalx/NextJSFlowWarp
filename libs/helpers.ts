896de353282344dfe041a18eb2f9eaab5c2dde84
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 04:48:22 +0400

    feat: post data helper

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -9,3 +9,28 @@ export const getUrl = () => {
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
   return url;
 };
+
+export const postData = async ({
+  url,
+  data,
+}: {
+  url: string;
+  data?: { price: Price };
+}) => {
+  console.log("posting,", url, data);
+
+  const res: Response = await fetch(url, {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    console.log("Error in postData", { url, data, res });
+
+    throw Error(res.statusText);
+  }
+
+  return res.json();
+};
