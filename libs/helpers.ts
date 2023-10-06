947bd73295bf48ccc78bd5c95d8f5dd686d9e2d4
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 04:49:30 +0400

    feat: toDateTime func helper

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -34,3 +34,9 @@ export const postData = async ({
 
   return res.json();
 };
+
+export const toDateTime = (secs: number) => {
+  var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start.
+  t.setSeconds(secs);
+  return t;
+};
