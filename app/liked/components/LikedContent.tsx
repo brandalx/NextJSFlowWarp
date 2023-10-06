c429f8e6b921ec6ca2dfb1768a4b1cce1628e0c8
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-06 04:15:02 +0400

    feat: favorite playlist works

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -2,6 +2,7 @@
 
 import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
 import { useRouter } from "next/navigation";
@@ -20,6 +21,8 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
     }
   }, [isLoading, user, router]);
 
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-3 w-full px-6 text-neutral-400 ">
@@ -33,7 +36,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <LikeButton songId={song.id} />
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
         </div>
       ))}
